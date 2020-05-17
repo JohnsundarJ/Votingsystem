6070d3db9fdf72dbe7ae6fc12652f49658b7efea
jjohnsundar <jjohnsundar9943@gmail.com>
2020-05-17 13:14:09 +0530

    Updates

---
+++ precache-manifest.0a6c6bc31ef940cd028d5d7b6de02403.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2740a4ab9636573f9dac24c76dd37338",
+    "url": "/Votingsystem/index.html"
+  },
+  {
+    "revision": "c0930d0ef0422849378e",
+    "url": "/Votingsystem/static/css/main.f6a98191.chunk.css"
+  },
+  {
+    "revision": "3ea81d82953b3c81b82c",
+    "url": "/Votingsystem/static/js/2.d2fba637.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Votingsystem/static/js/2.d2fba637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0930d0ef0422849378e",
+    "url": "/Votingsystem/static/js/main.4bf93d63.chunk.js"
+  },
+  {
+    "revision": "e4d7015ba6be28d5eed3",
+    "url": "/Votingsystem/static/js/runtime-main.50e4feec.js"
+  }
+]);
